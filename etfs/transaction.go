824e031607e818498d72db860212c7f9acb776a5
framp <framp@linux-tips-and-tricks.de>
2019-10-05 13:10:48 +0200

    Added license

--- etfs/transaction.go
+++ etfs/transaction.go
@@ -12,9 +12,24 @@ import (
 
 //######################################################################################################################
 //
-//    Extract JamMan Stereo WAV files from NAND dump
+//   Extract JamMan Stereo WAV files from NAND dump
 //
-//    Copyright (C) 2019 framp at linux-tips-and-tricks dot de
+//   No elegant and fast code because a lot of trial and error coding happened but finally it recovers all files
+//
+//   Copyright (C) 2019 framp at linux-tips-and-tricks dot de
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //#######################################################################################################################
 
